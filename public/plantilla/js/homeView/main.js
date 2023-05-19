1959c23a6dc0105b950850651210310490f739f1
CamiloCVC <camicastellanos0602@gmail.com>
2023-05-18 20:19:37 -0500

    Actualizacion mensaje de modal principal

--- public/plantilla/js/homeView/main.js
+++ public/plantilla/js/homeView/main.js
@@ -1,23 +1,52 @@
-
 // import "https://unpkg.com/swiper@7/swiper-bundle.min.js";
 import CreateModal from "../utils/modal.js";
 const inicio = () => CarrucelVideos();
 
 const CarrucelVideos = () => {
-  const registroHeka = document.querySelector("#registroHeka")
-  const novedadesEnvios = document.querySelector("#novedadesEnvios")
-  const cuentaBancaria = document.querySelector("#registrarCuentaBancaria")
-  const cotizacionGuias = document.querySelector("#cotizacionGuias")
+  const registroHeka = document.querySelector("#registroHeka");
+  const novedadesEnvios = document.querySelector("#novedadesEnvios");
+  const cuentaBancaria = document.querySelector("#registrarCuentaBancaria");
+  const cotizacionGuias = document.querySelector("#cotizacionGuias");
   const crearBodegas = document.querySelector("#creacionDeBodegas");
   const revisarPagos = document.querySelector("#revisarPagos");
 
-  datosModal(registroHeka, "https://www.youtube.com/embed/dLCJCvS7uWg?&autoplay=1", "Registro - Heka Entrega", "En este vídeo te enseñamos a como realizar el registro en nuestra plataforma de Heka Entrega.");
-  datosModal(novedadesEnvios, "https://www.youtube.com/embed/n6RpNdMThic?&autoplay=1", "¿Como saber las novedades de tus envíos?", "En este vídeo te enseñamos a gestionar tus novedades en nuestra plataforma de Heka Entrega.");
-  datosModal(cuentaBancaria, "https://www.youtube.com/embed/518tJQRPlXI?&autoplay=1", "Registro de cuenta bancaria", "En este vídeo te enseñamos a como registrar tu cuenta bancaria en nuestra plataforma de Heka Entrega.");
-  datosModal(cotizacionGuias, "https://www.youtube.com/embed/5tNUhNONIG4?&autoplay=1", "Cotización y creación de guías", "En este vídeo te enseñamos a generar tus guias en nuestra plataforma de Heka Entrega.");
-  datosModal(crearBodegas, "https://www.youtube.com/embed/ht54UmniYk0?&autoplay=1", "Registrar una bodega.", "En este vídeo te enseñamos a como registrar una bodega en nuestra plataforma de Heka Entrega.");
-  datosModal(revisarPagos, "https://www.youtube.com/embed/ACorbrGDJZo?&autoplay=1", "Revisar pagos.", "En este vídeo te enseñamos a cómo realizar la revisión de tus pagos en la plataforma de Heka entrega.");
-  
+  datosModal(
+    registroHeka,
+    "https://www.youtube.com/embed/dLCJCvS7uWg?&autoplay=1",
+    "Registro - Heka Entrega",
+    "En este vídeo te enseñamos a como realizar el registro en nuestra plataforma de Heka Entrega."
+  );
+  datosModal(
+    novedadesEnvios,
+    "https://www.youtube.com/embed/n6RpNdMThic?&autoplay=1",
+    "¿Como saber las novedades de tus envíos?",
+    "En este vídeo te enseñamos a gestionar tus novedades en nuestra plataforma de Heka Entrega."
+  );
+  datosModal(
+    cuentaBancaria,
+    "https://www.youtube.com/embed/518tJQRPlXI?&autoplay=1",
+    "Registro de cuenta bancaria",
+    "En este vídeo te enseñamos a como registrar tu cuenta bancaria en nuestra plataforma de Heka Entrega."
+  );
+  datosModal(
+    cotizacionGuias,
+    "https://www.youtube.com/embed/5tNUhNONIG4?&autoplay=1",
+    "Cotización y creación de guías",
+    "En este vídeo te enseñamos a generar tus guias en nuestra plataforma de Heka Entrega."
+  );
+  datosModal(
+    crearBodegas,
+    "https://www.youtube.com/embed/ht54UmniYk0?&autoplay=1",
+    "Registrar una bodega.",
+    "En este vídeo te enseñamos a como registrar una bodega en nuestra plataforma de Heka Entrega."
+  );
+  datosModal(
+    revisarPagos,
+    "https://www.youtube.com/embed/ACorbrGDJZo?&autoplay=1",
+    "Revisar pagos.",
+    "En este vídeo te enseñamos a cómo realizar la revisión de tus pagos en la plataforma de Heka entrega."
+  );
+
   new Swiper(".mySwiper", {
     slidesPerView: 2,
     spaceBetween: 10,
@@ -26,41 +55,43 @@ const CarrucelVideos = () => {
       // when window width is >= 480px
       480: {
         slidesPerView: 3,
-        spaceBetween: 15
+        spaceBetween: 15,
       },
       // when window width is >= 640px
       640: {
         slidesPerView: 4,
-        spaceBetween: 15
+        spaceBetween: 15,
       },
       1024: {
         slidesPerView: 5,
-        spaceBetween: 15
+        spaceBetween: 15,
       },
       1300: {
         slidesPerView: 6,
-        spaceBetween: 15
+        spaceBetween: 15,
       },
       1440: {
         slidesPerView: 7,
-        spaceBetween: 15
+        spaceBetween: 15,
       },
       1900: {
         slidesPerView: 10,
-        spaceBetween: 15
-      }
-    }
+        spaceBetween: 15,
+      },
+    },
   });
-}
+};
 
-const datosModal = (video, link, titulo, descripcion) => { 
+const datosModal = (video, link, titulo, descripcion) => {
   const modal = new CreateModal({
-    title: "<h4>"+titulo+"</h4>",
+    title: "<h4>" + titulo + "</h4>",
     modalSize: "modal-xl",
-  }); 
+  });
   const videoId = link.match(/(\/)([^#&\/]+)(\?)/)[2] || link;
 
-  video.querySelector("img").setAttribute("src", `http://img.youtube.com/vi/${videoId}/0.jpg`);
+  video
+    .querySelector("img")
+    .setAttribute("src", `http://img.youtube.com/vi/${videoId}/0.jpg`);
   video.addEventListener("click", (e) => {
     e.preventDefault();
     modal.init = `
@@ -71,8 +102,8 @@ const datosModal = (video, link, titulo, descripcion) => {
     `;
 
     modal.onSubmit = () => modal.close();
-  })
-}
+  });
+};
 
 modalInicial();
 function modalInicial() {
@@ -83,19 +114,32 @@ function modalInicial() {
 
   m.init = `
     <div class="text-center">
-      <img src="./img/logo-flexi.png" style="height: 30vh"/>
+      <img src="./img/logo-heka.png" style="height: 30vh"/>
+      <br/>
+      <h2> INFORMACION IMPORTANTE </h1>
     </div>
-    <h5>Flexii será una nueva forma de entregar mensajería nacional, será un producto creado por HEKA, con el fin entregar mensajería, por el momento estaremos en: <br/> <b>BOGOTÁ, CUCUTA,BUCARAMANGA,SILVANIA, FUSAGASUGÁ, GRANADA, CHINAUTA, ARBELAEZ.</b>
     <br/>
-    <br/>
-    Con los siguientes beneficios:</h5>
-    <p>✅ Avisaremos por WhatsApp al momento de entregar.</p>
-    <p>✅ En caso de que suceda una novedad el mensajero u oficina contacta con destinatario.</p>
-    <p>✅ Si destinatario no contesta, tienes hasta 15 días calendario para gestionar una novedad.</p>
-    <p>✅ Se realizaría devolución solamente si se autoriza o si tarda más de 15 días el paquete sin solucionar.</p>
-    <p>✅ Métodos de pago como: Nequi, Bancolombia, Daviplata, Davivienda.</p>
+    <h5>Si solicitaste tu pago el dia <b> 18 de mayo de 8:00 am a 6:00 pm</b> te pedimos un poco de paciencia puesto que el banco (PYMES) se encuentra caido, estaremos realizando el desembolso de las guias en el transcurso del dia tan pronto sea reestablecido el banco, posterior a ello se estaran subiendo los recuentos para actualizacion de billetera.</br> </h5>
+    <div class="text-center">
+      <h4><b>AGRADECEMOS TU COMPRENSION</b></h4>
+      <img src="./img/error_bancolombia.jpeg" style="height: 40vh"/>
+      <br/>
+    </div>
   `;
 
   m.onSubmit = () => m.close();
 }
 export default inicio;
+
+// <div class="text-center">
+//   <img src="./img/logo-flexi.png" style="height: 30vh"/>
+// </div>
+// <h5>Flexii será una nueva forma de entregar mensajería nacional, será un producto creado por HEKA, con el fin entregar mensajería, por el momento estaremos en: <br/> <b>BOGOTÁ, CUCUTA,BUCARAMANGA,SILVANIA, FUSAGASUGÁ, GRANADA, CHINAUTA, ARBELAEZ.</b>
+// <br/>
+// <br/>
+// Con los siguientes beneficios:</h5>
+// <p>✅ Avisaremos por WhatsApp al momento de entregar.</p>
+// <p>✅ En caso de que suceda una novedad el mensajero u oficina contacta con destinatario.</p>
+// <p>✅ Si destinatario no contesta, tienes hasta 15 días calendario para gestionar una novedad.</p>
+// <p>✅ Se realizaría devolución solamente si se autoriza o si tarda más de 15 días el paquete sin solucionar.</p>
+// <p>✅ Métodos de pago como: Nequi, Bancolombia, Daviplata, Davivienda.</p>
