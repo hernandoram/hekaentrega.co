a42aa2e75b42f1ff53a9018435e87412c67b0b4b
Stiven Romero <elromerostiven@gmail.com>
2024-02-19 07:58:50 -0500

    Se ha activado la creción de pedido a usuarios selectos

--- public/plantilla/js/cotizador.js
+++ public/plantilla/js/cotizador.js
@@ -1923,8 +1923,20 @@ function finalizarCotizacion(datos) {
 </div>`;
   let entrega_en_oficina = "";
 
+  const usuariosTemporalesPedido = [
+    "SellerLabodegadelcalzado",
+    "SellerBousni",
+    "SellerYussboreal",
+    "SellerVeryNiceCompras",
+    "SellerG-KKids",
+    "SellerVALENTINOSSTORE",
+    "SellerNuevo"
+  ];
+  const showCheckPedido = usuariosTemporalesPedido.includes(datos_usuario.centro_de_costo);
+  // Una vez que se pruebe la funcionnalidad y como de adaptan los usuarios ELIMINAR el d-none 
+  // con las variables que lo configuran (usuariosTemporalesPedido, showCheckPedido)
   const checkCreacionPedido = `
-        <div class="col-sm-6 mb-2 form-check d-none">
+        <div class="col-sm-6 mb-2 form-check ${showCheckPedido ? "" : "d-none"}">
             <input type="checkbox" id="check-crear_pedido" class="form-check-input">
             <label for="check-crear_pedido" class="form-check-label">Crear en forma de pedido</label>
         </div>
