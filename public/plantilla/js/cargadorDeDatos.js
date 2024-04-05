3a3e230ce3fc3763f25840ccd2313c3520e3c3b4
Jose Luis Bedoya <wildchamo@gmail.com>
2024-04-05 14:09:38 -0500

    update api link

--- public/plantilla/js/cargadorDeDatos.js
+++ public/plantilla/js/cargadorDeDatos.js
@@ -1,5 +1,5 @@
 //const PROD_API_URL = "https://api.hekaentrega.co"; //"https://apidev.hekaentrega.co" o esta
-const PROD_API_URL = "https://apidev.hekaentrega.co"; //comentar o descomentar segun el ambiente
+const PROD_API_URL = "https://api.hekaentrega.co"; //comentar o descomentar segun el ambiente
 // const PROD_API_URL_PLATFORM2 = "http://localhost:3232"; //comentar o descomentar segun el ambiente
 const PROD_API_URL_PLATFORM2 = "http://hekaentrega.co"; //comentar o descomentar segun el ambiente
 
