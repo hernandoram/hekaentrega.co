237cab0be81919e20450e25c865fba826fcdb41e
Stiven Romero <elromerostiven@gmail.com>
2024-05-28 08:53:34 -0500

    Se ha generado un cambio sobre la descarga del informe, añadiendo dos columnas para formatear la fecha de diferentes formas

--- public/plantilla/js/render.js
+++ public/plantilla/js/render.js
@@ -3022,7 +3022,6 @@ async function implantarEstadoNuevoAdm(guia, estadosGuia) {
     `,
     preConfirm: (data) => {
 
-      console.log("preconfirm: ", data);
       const form = document.getElementById("form-imp_estado");
       if(!form.checkValidity()) return Swal.showValidationMessage("Recuerde llenar los campos obligatorios.");
 
@@ -3823,6 +3822,58 @@ const segmentarArreglo = (arr, rango) => {
 
   return res;
 };
+
+const estandarizarFecha = (date, specialFormat, parseHour) => {
+  const fecha = new Date(date || new Date().getTime());
+  
+  if(isNaN(fecha.getTime())) return date;
+  
+  const norm = n => n < 10 ? "0" + n : n;
+  const format = {
+    D: fecha.getDate(),
+    DD: norm(fecha.getDate()),
+    M: fecha.getMonth() + 1,
+    MM: norm(fecha.getMonth() + 1),
+    YY: fecha.getFullYear().toString().slice(-2),
+    YYYY: fecha.getFullYear(),
+    H: fecha.getHours(),
+    HH: norm(fecha.getHours()),
+    m: fecha.getMinutes(),
+    mm: norm(fecha.getMinutes()),
+    s: fecha.getSeconds(),
+    ss: norm(fecha.getSeconds()),
+  }
+
+  let res = format.DD + "/" + format.MM + "/" + format.YYYY;
+  let originalHour = parseInt(format.H);
+  if(parseHour) {
+    let h = originalHour
+    h = h ? h : 12 
+    const hourParser = h > 12 ? h - 12 : h
+    format.HH = norm(hourParser);
+    format.H = hourParser;
+  }
+
+  if(specialFormat) {
+    res = "";
+    const str = specialFormat.match(/(\w+)/g);
+    const sign = specialFormat.match(/([^\w+])/g);
+
+    str.forEach((v,i) => {
+      res += format[v];
+      if(sign && sign[i]) {
+        res += sign[i];
+      }
+    });
+
+    if(parseHour) {
+      res += originalHour > 12 ? "p.m" : "a.m";
+    }
+    
+  }
+
+  return res;
+}
 
 class DetectorErroresInput {
   constructor(...selectors) {
