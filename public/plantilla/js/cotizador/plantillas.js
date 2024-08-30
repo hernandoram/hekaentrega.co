dd0c078a9cefef891534004f1c780214ec145089
Jose Luis <wildchamo@gmail.com>
2024-08-30 10:25:24 -0500

    depure consoleLog

--- public/plantilla/js/cotizador/plantillas.js
+++ public/plantilla/js/cotizador/plantillas.js
@@ -62,7 +62,6 @@ export function llenarProductos(num) {
 
     q.forEach((d) => {
       const data = d.data();
-      console.log(data);
       if (data.eliminada) return;
 
       const ciudadBusqueda = ciudades.find(
