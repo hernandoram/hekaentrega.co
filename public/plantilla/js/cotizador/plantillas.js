5648600dc52f1adca42206775c28cdd49341ac1c
Json Jimenez <jeisonjimenes.1998@gmail.com>
2024-09-03 10:48:17 -0500

    normalice

--- public/plantilla/js/cotizador/plantillas.js
+++ public/plantilla/js/cotizador/plantillas.js
@@ -115,16 +115,18 @@ function setearCiudad(inp, data) {
 
 function buscarCiudad(el, ciudad) {
   if (!ciudad) return;
-
+  const ciudadString = ciudad.normalize("NFD") // Descompone los caracteres acentuados en sus partes (e.g., "á" -> "á")
+  .replace(/[\u0300-\u036f]/g, "") // Elimina los caracteres diacríticos (las tildes)
+  .toUpperCase()
   charger.init();
-  if (ciudadesTomadas.has(ciudad)) {
-    return setearCiudad(el, ciudadesTomadas.get(ciudad));
+  if (ciudadesTomadas.has(ciudadString)) {
+    return setearCiudad(el, ciudadesTomadas.get(ciudadString));
   }
 
-  console.warn(ciudad);
+  console.warn(ciudadString);
 
   db.collection("ciudades")
-    .where("nombre", "==", ciudad)
+    .where("nombre", "==", ciudadString)
     .limit(3)
     .get()
     .then((q) => {
