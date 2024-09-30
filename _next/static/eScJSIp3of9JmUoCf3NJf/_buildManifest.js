e38501ca246b8fcaa22291fb3cbb451a8f04b928
AdrianMvpDev <adrianimpor1@gmail.com>
2024-09-30 09:52:35 -0500

    Ajuste pantallas grandes

---
+++ _next/static/eScJSIp3of9JmUoCf3NJf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/79-e40f1d35038df50e.js","static/css/1bfa1d48b1759266.css","static/chunks/pages/index-81cdd8c80d2fd1ec.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
