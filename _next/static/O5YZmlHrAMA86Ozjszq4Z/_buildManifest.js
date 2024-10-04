7c2334e5584b20f97670eb4c97e4c94317b04162
AdrianMvpDev <adrianimpor1@gmail.com>
2024-10-04 11:00:39 -0500

    Ajuste feedback

---
+++ _next/static/O5YZmlHrAMA86Ozjszq4Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/79-e40f1d35038df50e.js","static/css/2549da26700aee88.css","static/chunks/pages/index-231a3f0ec3a930d7.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
