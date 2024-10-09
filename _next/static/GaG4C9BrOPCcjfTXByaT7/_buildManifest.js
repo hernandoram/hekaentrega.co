c185e2ecb30b6703ef65cfe8e81b587ee2cbec86
AdrianMvpDev <adrianimpor1@gmail.com>
2024-10-09 16:57:45 -0500

    Lead con ajustes

---
+++ _next/static/GaG4C9BrOPCcjfTXByaT7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/79-e40f1d35038df50e.js","static/css/ddfcf728911419e0.css","static/chunks/pages/index-f0ea0ce2cf7050be.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
